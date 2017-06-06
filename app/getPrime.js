23ed413e63720f7d4669aa239729ca30101dc216
omolewa <akinyomiomolewa30gmail.com>
2017-06-06 02:17:16 +0100

    added two files

---
+++ app/getPrime.js
@@ -0,0 +1,20 @@
+var exports = module.export.{};
+function (num){
+	let arr = [];
+	for (let i=2; i <=num; i++){
+	if (i % 2 == 0 $$ i > 2){
+	continue;
+	let prime = true;
+	for (let count = 2; count <= math.trunc(math.sqrt(i)); count++){
+	if (i % count == 0){
+	prime = false;
+	break
+	      }
+	   }
+	   if prime(i){
+	   array.push(i);
+	      }
+	   }
+    return array
+	}
+}
---
+++ test/test.js
@@ -0,0 +1,36 @@
+// testing code goes here
+
+'use strict';
+const myApp = require('../app/getPrime.js');
+
+describe ("getting prime numbers", function (){
+it("should return 2,3 as prime for 5", function(){
+expect (myApp.getPrime(5)).toBe(2,3,5);
+})
+it("should return 2,3,5,7 as prime for 7 " function(){
+  expect(myApp.getPrime(7)).toBe(2,3,5,7);
+})
+it("should return 2,3,5,7 as prime for 10" function(){
+expect(myApp.getPrime(10)).toBe(2,3,5,7);
+})
+it("should return 2,3,5,7 as prime for 9" function(){
+expect(myApp.getPrime(9)).toBe(2,3,5,7);
+})
+it("should return undefined as prime for 0", function(){
+expect(myApp.getPrime(0)).toBe(Undefined);
+})
+it("should return undefined as prime for -1", function(){
+expect(myApp.getPrime(-1)).toBe(Undefined);
+})
+it("should return undefined as prime for 1", function(){
+expect(myApp.getPrime(1)).toBe(Undefined);
+})
+it("should return undefined as prine for NaN", function(){
+expect(myApp.getPrime(NaN)).toBe(Undefined);
+})
+it("should return 2,3,5,7,11 as prime for 12", function(){
+expect(myApp.getPrime(12)).toBe(2,3,5,7,11);
+})
+});
+
+
